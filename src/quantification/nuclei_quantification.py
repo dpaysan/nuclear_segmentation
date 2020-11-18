042155e4d612bfa88bd051b5e07f3b01356533e1
dpaysan <daniel.paysan@gmail.com>
2020-11-18 09:56:58 +0100

    restructuring

---
+++ src/quantification/nuclei_quantification.py
@@ -0,0 +1,4 @@
+from skimage import measure
+import numpy as np
+import pandas as pd
+
--- src/run_scripts/filter_out_slides.py
+++ src/run_scripts/filter_out_slides.py
@@ -5,13 +5,13 @@ from shutil import copyfile
 import sys
 
 
-output_dir = '../../data/tcell_project/split_raw_data/filtered_splits/tzyxc_pkl'
+output_dir = '../../data/tcell_project/human_cd4/split_raw_data/filtered_splits/tzyxc_pkl'
 os.makedirs(output_dir, exist_ok=True)
-data_infos = pd.read_csv('../../data/tcell_project/data_list.csv')
+data_infos = pd.read_csv('../../data/tcell_project/human_cd4/data_list.csv')
 nan_rows = data_infos.iloc[:,1:].isnull().all(axis=1)
 filtered_slice_names = data_infos.loc[nan_rows].iloc[:,0]
 
-all_files = get_file_list('../../data/tcell_project/split_raw_data/unfiltered_splits/tzyxc_pkl/pkl_files')
+all_files = get_file_list('../../data/tcell_project/human_cd4/split_raw_data/unfiltered_splits/tzyxc_pkl/pkl_files')
 filtered_files = []
 for file in all_files:
     short_file_name = os.path.split(file)[1]
